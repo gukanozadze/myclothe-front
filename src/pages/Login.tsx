b2819f0583f1ed6da2344eccefd5346f2c172427
Guka Nozadze <gukanozadze@gmail.com>
2022-04-01 02:19:07 +0400

    removed required for login and password

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -57,7 +57,6 @@ const Login = () => {
 									name='email'
 									type='email'
 									autoComplete='email'
-									required
 									value={email}
 									onChange={e => setEmail(e.target.value)}
 									className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
@@ -77,7 +76,6 @@ const Login = () => {
 									autoComplete='current-password'
 									value={password}
 									onChange={e => setPassword(e.target.value)}
-									required
 									className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
 								/>
 							</div>
